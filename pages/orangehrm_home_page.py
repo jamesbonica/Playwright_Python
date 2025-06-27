507263c53ac6ba340a3cb3c26ecf26db57f20d22
James Bonica <43998629+jamesbonica@users.noreply.github.com>
2025-06-26 20:43:08 -0400

    page objects added and use demonstrated (#5)

---
+++ pages/orangehrm_home_page.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+class HomePage:
+
+    def __init__(self,page:Page):
+        self.page = page
+        self.upgrade_button = page.get_by_role('button', name='Upgrade')
+        self.performance_link = page.get_by_role('link', name='Performance')
+        self.dashboard_link = page.get_by_role('link', name='Dashboard')
+
+
+    def click_performance(self):
+        self.performance_link.click()
+
+    def click_dashboard(self):
+        self.dashboard_link.click()
